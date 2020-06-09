94b0013330eb33a48b2fcc076bf070dc07680ceb
haichen <phcbest2017@outlook.com>
2020-06-09 22:21:18 +0800

    第一次使用黄油刀

--- app/src/main/java/com/phc/neckrreferential/ui/activity/MainActivity.java
+++ app/src/main/java/com/phc/neckrreferential/ui/activity/MainActivity.java
@@ -1,35 +1,67 @@
 package com.phc.neckrreferential.ui.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.phc.neckrreferential.R;
 import com.phc.neckrreferential.ui.fragment.HomeFragment;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
-    private FrameLayout mainPageContainer;
+    private static final String TAG = "MainActivity";
+
+    public FrameLayout mainPageContainer;
+
+    @BindView(R.id.main_navigation_bar)
+    public BottomNavigationView mainNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
         initView();
+        initListener();
 
+    }
 
+    private void initListener() {
+        mainNavigationBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.home){
+                    Log.d(TAG, "切换到了home");
+                }else if (item.getItemId() == R.id.selected){
+                    Log.d(TAG, "切换到了selected");
+                }else if (item.getItemId() == R.id.red_packet){
+                    Log.d(TAG, "切换到了red_packet");
+                }else if (item.getItemId() == R.id.search){
+                    Log.d(TAG, "切换到了search");
+                }
+                return true;
+            }
+        });
     }
 
     private void initView() {
         //user transaction add fragment
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.add(R.id.main_page_container,new HomeFragment());
+        ft.add(R.id.main_page_container, new HomeFragment());
         ft.commit();
 
         mainPageContainer = (FrameLayout) findViewById(R.id.main_page_container);
+
     }
 }
---
+++ app/src/main/java/com/phc/neckrreferential/utils/LogUtils.java
@@ -0,0 +1,13 @@
+package com.phc.neckrreferential.utils;
+
+/**
+ * 版权：没有版权 看得上就用
+ *
+ * @author peng
+ * 创建日期：2020/6/9 22
+ * 描述： log的可管控的工具类
+ */
+public class LogUtils {
+    public static int currentLev = 4 ;
+
+}
